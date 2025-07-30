91e1cd1255dd79d6039dffee234cbd5bbe077556
Erlan <erlasha07@gmail.com>
2025-07-30 23:31:33 +0500

    Изменен renderAddPostPageComponent

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,21 +1,50 @@
+import { renderHeaderComponent } from './header-component.js'; // Возвращаем импорт компонента заголовка
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
-    // @TODO: Реализовать страницу добавления поста
-    const appHtml = `
-    <div class="page-container">
-      <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
-    </div>
-  `;
+    appEl.innerHTML = `
+      <div class="page-container">
+        <div class="header-container"></div> <div class="form">
+          <h3 class="form-title">Добавить пост</h3>
+          <div class="form-inputs">
+            <div class="upload-image-container">
+              <div class="upload=image"> <label class="file-upload-label secondary-button">
+                    <input type="file" id="file-input" class="file-upload-input" style="display:none">
+                    Выберите фото
+                </label>
+              </div>
+            </div>
+            <label>
+              Опишите фотографию:
+              <textarea class="input textarea" data-post-description rows="4"></textarea>
+            </label>
+            <button class="button" id="add-button">Добавить</button>
+          </div>
+        </div>
+      </div>
+    `;
 
-    appEl.innerHTML = appHtml;
+    // Шапка
+    renderHeaderComponent({
+      element: document.querySelector('.header-container'),
+    });
 
-    document.getElementById("add-button").addEventListener("click", () => {
+    document.getElementById('add-button').addEventListener('click', () => {
+      const description = appEl.querySelector('[data-post-description]').value;
+      const imageUrl = ''; // Реализация URL
+
       onAddPostClick({
-        description: "Описание картинки",
-        imageUrl: "https://image.png",
+        description: description,
+        imageUrl: imageUrl,
       });
+    });
+
+    const fileInput = appEl.querySelector('#file-input'); // Для диалогового окна в дальнейшем
+    fileInput.addEventListener('change', (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        console.log('Выбран файл:', file.name); // Проверка
+      }
     });
   };
 
